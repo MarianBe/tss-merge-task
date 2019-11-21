91e51e6009db9e23bb9c20f3efa2cc4499f24fee
Marian Beuther <marian.beuther@peakconcepts.de>
2019-11-21 12:04:23 +0100

    eslint, prettier, precommit hooks, components added loader added addRandomData added

--- src/App.js
+++ src/App.js
@@ -1,43 +1,65 @@
-import React from "react";
-import Chip from "@material-ui/core/Chip";
-import TextField from "@material-ui/core/TextField";
-import Button from "@material-ui/core/Button";
-import Container from "@material-ui/core/Container";
-import Grid from "@material-ui/core/Grid";
-import "./App.css";
-import styled from "styled-components";
-import Header from "components/header";
-const InnerContainer = styled.div`
-  width: 100%;
-  height: 100%;
+import React, { useState } from 'react'
+import Grid from '@material-ui/core/Grid'
+import './App.css'
+import styled from 'styled-components'
+import Header from 'components/header'
+import Chips from 'components/chips'
+import Loader from 'components/loader'
+import AddInput from 'components/add-input'
+const Container = styled.div`
   display: flex;
   flex: 1;
-  padding: 30px;
-`;
+  flex-direction: column;
+`
 function App() {
+  const [data, setData] = useState([])
+  const [loading, setLoading] = useState(false)
+
+  const addData = element => {
+    const newData = [...data, element]
+    setData(newData)
+  }
+
+  const loadRandomSet = async () => {
+    setLoading(true)
+    setTimeout(() => {
+      const data = Array.from({ length: 1000 }, () => [
+        Math.floor(Math.random() * 1000),
+        Math.floor(Math.random() * 1000)
+      ])
+      data.forEach(innerArray => {
+        if (innerArray[0] > innerArray[1]) {
+          const memory = innerArray[0]
+          innerArray[0] = innerArray[1]
+          innerArray[1] = memory
+        }
+      })
+      setData(data)
+      setLoading(false)
+    }, 1)
+  }
+
   return (
     <Container>
+      {loading && <Loader />}
       <Header />
       <Grid container spacing={1}>
-        <Grid container item xs={12} spacing={3}>
-          <TextField
-            id="standard-number"
-            label="Zahlen mit Komma getrennt"
-            InputLabelProps={{
-              shrink: true
-            }}
-            margin="normal"
-          />
-          <Button variant="contained" color="primary">
-            Hinzufügen
-          </Button>
+        <Grid container item xs={12}>
+          <AddInput addData={addData} loadRandomSet={loadRandomSet} />
         </Grid>
-        <Grid container item xs={12} spacing={3}>
-          <Chip label="[0, 10]" onDelete={() => console.log("delete")} />
+        <Grid container item xs={12}>
+          <Chips
+            title="Eingabe"
+            data={data}
+            onDelete={element => console.log('delete', element)}
+          />
+        </Grid>
+        <Grid container item xs={12}>
+          <Chips title="Ausgabe" data={data} />
         </Grid>
       </Grid>
     </Container>
-  );
+  )
 }
 
-export default App;
+export default App
